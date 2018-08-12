92313752fab517a83d7a11cf74f94942efb9e410
Rafael Calunga <rafaelcalunga@gmail.com>
2018-08-11 21:23:19 -0400

    Initial Commit

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var startRecordingButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        stopRecordingButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func startRecording(_ sender: UIButton) {
+        recordingLabel.text = "Recording in Progress"
+        startRecordingButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
+    }
+    
+    @IBAction func stopRecording(_ sender: UIButton) {
+        stopRecordingButton.isEnabled = false
+        startRecordingButton.isEnabled = true
+        recordingLabel.text = "Tap to record"
+    }
+    
 }
-
